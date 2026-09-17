1a2e1b667413506254a68a47e054b500520311d9
rajthakkar-vml <raj.thakkar@vml.com>
2026-09-17 13:19:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -66,7 +66,7 @@ function aistudioMediaPlugin(): Plugin {
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: './gp-mode-qualifiers/',
     plugins: [react(), tailwindcss(), aistudioMediaPlugin()],
     resolve: {
       alias: {
